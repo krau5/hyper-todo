298e44925265a6499ef5df41d21d7c3132c4b631
krau5 <apvbd17@gmail.com>
2025-02-03 23:22:46 +0100

    Update domain models with gorm struct tags

--- domain/user.go
+++ domain/user.go
@@ -4,9 +4,9 @@ import "time"
 
 type User struct {
 	ID        int64     `json:"-" gorm:"unique;autoIncrement"`
-	Name      string    `json:"name"`
+	Name      string    `json:"name" gorm:"not null"`
 	Email     string    `json:"email" gorm:"unique;not null"`
-	Password  string    `json:"-"`
+	Password  string    `json:"-" gorm:"not null"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 }
